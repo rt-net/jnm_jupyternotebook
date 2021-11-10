ef3d39741eb228013bb6f4762b9bc0be6d09e4bf
Daisuke Sato <daisuke.sato@rt-net.jp>
2021-11-10 09:40:50 +0900

    Fix import error

--- jnmouse/__init__.py
+++ jnmouse/__init__.py
@@ -4,4 +4,5 @@ from .motor import Motor
 from .robot import Robot
 from .image import bgr8_to_jpeg
 from .object_detection import ObjectDetector
+from .object_detection import PycudaObjectDetector
 from ._version import __version__  # noqa:F401
